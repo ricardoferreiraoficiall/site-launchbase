1654445e3811395e167284cbc0a24ac5892a8b90
Ricardo Ferreira <ricardoferreiraoficial01@gmail.com>
2020-06-16 19:39:42 -0300

    dynamically searching for videos

--- scripts.js
+++ scripts.js
@@ -4,9 +4,13 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards) {
     card.addEventListener("click", function() {
         modalOverlay.classList.add('active');
+        const video_id = card.getAttribute('id');
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${video_id}`;
     });
 }
 
 document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove('active');
+    modalOverlay.querySelector('iframe').src = '';
 });
+
